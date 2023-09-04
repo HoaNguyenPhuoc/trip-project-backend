0f1f88c9d31c7988726d920b92c9261d697723f4
HoaNguyen <nguyenhoapyu@gmail.com>
2023-09-04 20:59:56 +0700

    Initial commit

---
+++ src/main/java/com/apps/trip/config/OpenAPIConfiguration.java
@@ -0,0 +1,26 @@
+package com.apps.trip.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("JWT",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER).name("Authorization")))
+                .info(new Info().title("Trips App API").version("snapshot"))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("JWT", Arrays.asList("read", "write")));
+    }
+}
