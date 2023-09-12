026c210bb6bc2454890f0cadd5243c6ca3885abd
HoaNguyen <nguyenhoapyu@gmail.com>
2023-09-12 20:50:24 +0700

    [develop] update openapi

--- src/main/java/com/apps/trip/config/OpenAPIConfiguration.java
+++ src/main/java/com/apps/trip/config/OpenAPIConfiguration.java
@@ -1,5 +1,7 @@
 package com.apps.trip.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
 
 @Configuration
+@OpenAPIDefinition(servers = @Server(url = "https://trip-project.up.railway.app/"))
 public class OpenAPIConfiguration {
 
     @Bean
